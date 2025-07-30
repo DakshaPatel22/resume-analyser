8eb5fb3f940758fac8efd1b26d56dbfeae0080a4
Daksha Patel <kumarvithal2211@gmail.com>
2025-07-30 14:20:12 +0530

    Navbar and card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Resume Check karalo" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
-
+  
 export default function Home() {
-  return <Welcome />;
+  return(
+
+  <main className="bg-[url('/images/bg-main.svg')]">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+         <h1>Track Your Application & resume ratings </h1>
+         <h2> Review submission and check ai powered feedbacks</h2>
+      </div>
+    </section>
+    {resumes.length>0 &&( 
+    <div className="resumes-section">
+      { resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume}/>
+          ))}
+          </div>
+        )} 
+    </main>
+  )
 }
